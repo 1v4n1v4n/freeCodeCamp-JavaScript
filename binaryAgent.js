c9f2dbe5562a67821c43ed4f2b83319c2bb4fc67
Ivan <45673809+1v4n1v4n@users.noreply.github.com>
2020-06-30 01:46:09 -0500

    Add files via upload

---
+++ binaryAgent.js
@@ -0,0 +1,10 @@
+function binaryAgent(str) {
+  var newArr = str.split(" ");
+  var newStr = "";
+  for (let i = 0; i < newArr.length; i++) {
+    newStr += String.fromCharCode(parseInt(newArr[i], 2));
+  }
+  return newStr;
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
